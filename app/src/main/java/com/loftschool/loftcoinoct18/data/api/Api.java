86c5a755bc1d6618d08a7112c598de2d08e7f8ce
Renat Shakhmatov <shushper@gmail.com>
2018-10-16 00:53:29 +0500

    Homework6

--- app/src/main/java/com/loftschool/loftcoinoct18/data/api/Api.java
+++ app/src/main/java/com/loftschool/loftcoinoct18/data/api/Api.java
@@ -2,12 +2,12 @@ package com.loftschool.loftcoinoct18.data.api;
 
 import com.loftschool.loftcoinoct18.data.api.model.RateResponse;
 
-import retrofit2.Call;
+import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface Api {
 
     @GET("ticker")
-    Call<RateResponse> ticker(@Query("structure") String structure, @Query("convert") String convert);
+    Observable<RateResponse> ticker(@Query("structure") String structure, @Query("convert") String convert);
 }
